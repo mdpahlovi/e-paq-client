7b4950f2fbf4229650bd1918a14466b663d6be2f
MD Pahlovi <mdpahlovi07@gmail.com>
2022-10-18 22:22:21 +0600

    Toastify Added on signup

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -27,34 +27,32 @@ const LogIn = () => {
     };
 
     return (
-        <section onSubmit={handelSubmit} className="section-gap flex justify-center items-center">
-            <div className="w-full px-6 xs:w-[448px]">
+        <section onSubmit={handelSubmit} className="section-gap flex justify-center">
+            <div className="w-full px-6 xs:w-[448px] content-gap">
+                <div className="flex items-center gap-3">
+                    <p className="text-lg">Sign In with :</p>
+                    <button className="btn btn-circle btn-primary text-2xl">
+                        <BsGoogle />
+                    </button>
+                    <button className="btn btn-circle btn-primary text-2xl">
+                        <BsFacebook />
+                    </button>
+                    <button className="btn btn-circle btn-primary text-2xl">
+                        <BsGithub />
+                    </button>
+                </div>
+                <div className="vertical-line">
+                    <p className="text-center font-semibold mx-4 mb-0">Or</p>
+                </div>
                 <form className="content-gap">
-                    <div className="flex flex-row items-center gap-3">
-                        <p className="text-lg mb-0 mr-4">Sign In with :</p>
-                        <button className="btn btn-circle btn-primary text-2xl">
-                            <BsGoogle />
-                        </button>
-                        <button className="btn btn-circle btn-primary text-2xl">
-                            <BsFacebook />
-                        </button>
-                        <button className="btn btn-circle btn-primary text-2xl">
-                            <BsGithub />
-                        </button>
-                    </div>
-                    <div className="flex items-center before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
-                        <p className="text-center font-semibold mx-4 mb-0">Or</p>
-                    </div>
-                    <input type="email" name="email" placeholder="Email" className="mb-1 input input-primary w-full" required />
-                    <input type="password" name="password" placeholder="Password" className="input input-primary w-full" required />
+                    <input type="email" name="email" placeholder="Email" className="mb-2 input-field" required />
+                    <input type="password" name="password" placeholder="Password" className="input-field" required />
                     <div className="flex justify-between items-center">
-                        <div className="form-control">
-                            <label className="label cursor-pointer">
-                                <span className="label-text mr-2">Remember me</span>
-                                <input type="checkbox" className="checkbox checkbox-primary" />
-                            </label>
-                        </div>
-                        <a href="#!">Forgot password?</a>
+                        <label className="label cursor-pointer">
+                            <span className="label-text mr-2">Remember me</span>
+                            <input type="checkbox" className="checkbox checkbox-primary" />
+                        </label>
+                        <Link>Forgot password?</Link>
                     </div>
                     <button className="btn btn-primary">LogIn</button>
                     <p className="text-sm font-semibold">
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,25 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { BsGoogle, BsFacebook, BsGithub } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import { AuthContext } from "../context/UserContext";
+import { getThemeValue } from "../Utilities/getThemeValue";
 
 const SignUp = () => {
-    const [error, setError] = useState(null);
     const { createUser } = useContext(AuthContext);
+
+    const notify = (error) => {
+        toast.error(`OPPS ! ${error}`, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: getThemeValue(),
+        });
+    };
 
     const handelSubmit = (event) => {
         event.preventDefault();
@@ -15,66 +29,56 @@ const SignUp = () => {
         const password = form.password.value;
         const confirmPassword = form.confirmPassword.value;
 
-        console.log(name);
-
         if (password.length < 6) {
-            setError("PassWord sould be 6 cherecter or more");
+            notify("Password sould be 6 cherecter or more");
             return;
         }
         if (password !== confirmPassword) {
-            setError("Your Password Didn't Match");
+            notify("Your password didn't match");
             return;
         }
 
         createUser(email, password)
             .then((result) => {
                 const user = result.user;
-                console.log(user);
+                user.displayName = name;
                 form.reset();
             })
             .catch((error) => console.error(error));
     };
 
     return (
-        <section className="section-gap flex justify-center items-center">
-            <div className="w-full px-6 xs:w-[448px]">
+        <section className="section-gap flex justify-center">
+            <div className="w-full px-6 xs:w-[448px] content-gap">
+                <div className="flex items-center gap-3">
+                    <p className="text-lg">Sign Up with :</p>
+                    <button className="btn btn-circle btn-primary text-2xl">
+                        <BsGoogle />
+                    </button>
+                    <button className="btn btn-circle btn-primary text-2xl">
+                        <BsFacebook />
+                    </button>
+                    <button className="btn btn-circle btn-primary text-2xl">
+                        <BsGithub />
+                    </button>
+                </div>
+                <div className="vertical-line">
+                    <p className="text-center font-semibold mx-4 mb-0">Or</p>
+                </div>
                 <form onSubmit={handelSubmit} className="content-gap">
-                    <div className="flex flex-row items-center gap-3">
-                        <p className="text-lg mb-0 mr-4">Sign Up with :</p>
-                        <button className="btn btn-circle btn-primary text-2xl">
-                            <BsGoogle />
-                        </button>
-                        <button className="btn btn-circle btn-primary text-2xl">
-                            <BsFacebook />
-                        </button>
-                        <button className="btn btn-circle btn-primary text-2xl">
-                            <BsGithub />
-                        </button>
+                    <div className="flex flex-col xs:flex-row gap-5 mb-2">
+                        <input type="text" name="fastName" placeholder="Fast Name" className="input-field" />
+                        <input type="text" name="lastName" placeholder="Last Name" className="input-field" />
                     </div>
-                    <div className="flex items-center before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
-                        <p className="text-center font-semibold mx-4 mb-0">Or</p>
-                    </div>
-                    <div className="flex flex-col xs:flex-row gap-3">
-                        <input type="text" name="fastName" placeholder="Fast Name" className="mb-1 input input-primary w-full" />
-                        <input type="text" name="lastName" placeholder="Last Name" className="mb-1 input input-primary w-full" />
-                    </div>
-                    <input type="email" name="email" placeholder="Email" className="mb-1 input input-primary w-full" required />
-                    <input type="password" name="password" placeholder="Password" className="input input-primary w-full" required />
-                    <input
-                        type="password"
-                        name="confirmPassword"
-                        placeholder="Confirm Password"
-                        className="input input-primary w-full"
-                        required
-                    />
+                    <input type="email" name="email" placeholder="Email" className="mb-2 input-field" required />
+                    <input type="password" name="password" placeholder="Password" className="mb-2 input-field" required />
+                    <input type="password" name="confirmPassword" placeholder="Confirm Password" className="input-field" required />
                     <div className="flex justify-between items-center">
-                        <div className="form-control">
-                            <label className="label cursor-pointer">
-                                <span className="label-text mr-2">Remember me</span>
-                                <input type="checkbox" className="checkbox checkbox-primary" />
-                            </label>
-                        </div>
-                        <a href="#!">Forgot password?</a>
+                        <label className="label cursor-pointer">
+                            <span className="label-text mr-2">Remember me</span>
+                            <input type="checkbox" className="checkbox checkbox-primary" />
+                        </label>
+                        <Link>Forgot password?</Link>
                     </div>
                     <input type="submit" value="Sign Up" className="btn btn-primary" />
                     <p className="text-sm font-semibold">
@@ -83,7 +87,6 @@ const SignUp = () => {
                             LogIn
                         </Link>
                     </p>
-                    <p>{error}</p>
                 </form>
             </div>
         </section>
